ae90d88024c6315b78aef918d6c08b253cc1e0de
Mono Raptor <monoraptor29@gmail.com>
2023-04-12 08:10:45 +0100

    Fix global-stats formatting error, correct leaderBoard to leaderboard in list-commands

--- model/info.ts
+++ model/info.ts
@@ -1,6 +1,6 @@
 async function createCommandsMessage() {
   let message = '';
-  message += `**/leaderBoard** Show the leaderboard overall or for a specific skill \n`;
+  message += `**/leaderboard** Show the leaderboard overall or for a specific skill \n`;
   message += `**/player** Show info about a player \n`;
   message += `**/global-stats** Show global stats \n`;
   message += `**/list-commands** Shows list of bot commands \n`;
